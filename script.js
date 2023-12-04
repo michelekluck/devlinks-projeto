9ead9a659246485871b0ab186b21d82ec6eed266
michelekluck <140463604+michelekluck@users.noreply.github.com>
2023-12-04 20:40:14 -0300

    adicionar foto minha

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toggleMode() {
   // substituir a imagem
   if(html.classList.contains("light")) { 
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-michele-light.png")
     // setAttribute = modifica um atributo
   } else { 
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-michele.png")
   }
 
   if(html.classList.contains("light")) {
