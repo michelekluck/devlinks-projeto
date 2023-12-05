e9c203d4f0f83dff225b2bc8c5e98d1d561d04a6
michelekluck <140463604+michelekluck@users.noreply.github.com>
2023-12-04 21:04:10 -0300

    atualizar nome imagens

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toggleMode() {
   // substituir a imagem
   if(html.classList.contains("light")) { 
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-michele-light.png")
+    img.setAttribute("src", "./assets/avatar-michele-light-2.png")
     // setAttribute = modifica um atributo
   } else { 
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar-michele.png")
+    img.setAttribute("src", "./assets/avatar-michele-1.png")
   }
 
   if(html.classList.contains("light")) {
